d366ed3e2baa7726c2ac584198d613889b218c17
Andrés Ordóñez <theandtor@gmail.com>
2019-07-03 02:50:03 -0500

    Added global constant for storage permission schedules for 'Pico y Placa'

---
+++ src/constants/global-constants.ts
@@ -0,0 +1,10 @@
+// This constant allows you to store the "pico y placa" schedule list for days
+export const LIST_OF_LICESE_PLATE_END_DIGIT_ALLOWED_PER_DAY = {
+    MONDAY: [1, 2],
+    TUESDAY: [3, 4],
+    WEDNESDAY: [5, 6],
+    THURSDAY: [7, 8],
+    FRIDAY: [9, 0],
+    SATURDAY: [],
+    SUNDAY: []
+}
